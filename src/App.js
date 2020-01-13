4496ddbb067725aedec9fe36e48e5773d719a49e
Evgeniy Kononko <evgeniy.escape@gmail.com>
2020-01-14 00:18:48 +0200

    add on main app page Users & Pagination components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Users from './components/Users';
+import Pagination from './components/Pagination';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="container">
+			<h1>Users</h1>
+			<hr />
+			<Pagination />
+			<Users />
+		</div>
+	);
 }
 
 export default App;
