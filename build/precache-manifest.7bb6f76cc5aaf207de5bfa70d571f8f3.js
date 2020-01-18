4dd861aef6eedd1d8d29614c4f6347c792b4c4df
Evgeniy Kononko <evgeniy.escape@gmail.com>
2020-01-18 17:54:04 +0200

    add prod

---
+++ build/precache-manifest.7bb6f76cc5aaf207de5bfa70d571f8f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "863fa12f258dda33d0825b3f0adfc918",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cdbfd053775e9b67ea5",
+    "url": "/static/css/main.45761385.chunk.css"
+  },
+  {
+    "revision": "84c5e6617027753911e7",
+    "url": "/static/js/2.afd659cc.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.afd659cc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8cdbfd053775e9b67ea5",
+    "url": "/static/js/main.f958998c.chunk.js"
+  },
+  {
+    "revision": "9b9da2c3d813e524fd79",
+    "url": "/static/js/runtime-main.739cb692.js"
+  }
+]);
